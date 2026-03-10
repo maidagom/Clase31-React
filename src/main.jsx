72e4abda5bf6644fe2e57a5116a578a91e026502
Maida Gómez <maidagompin@gmail.com>
2026-03-10 16:44:19 -0300

    se agrego el strictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {App} from './App.jsx'
 
-createRoot(document.querySelector('#root')).render(<><App nombre="Juan" /></>,)
+createRoot(document.querySelector('#root')).render(<><StrictMode><App /></StrictMode></>,)
